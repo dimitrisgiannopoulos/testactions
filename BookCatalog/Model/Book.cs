e4575475e83580fa7819ac050e7cfa3883f441ce
Dimitris Giannopoulos <30804541+dimitrisgiannopoulos@users.noreply.github.com>
2022-08-19 18:10:44 +0300

    Delete BookCatalog directory

--- BookCatalog/Model/Book.cs
+++
@@ -1,9 +0,0 @@
-namespace BookCatalog.Models
-{
-    public class Book
-    {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public decimal Price { get; set; }
-    }
-}
