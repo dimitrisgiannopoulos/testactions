6a6ca06fcad4b6e40694b310eabbbf0404fa2c4c
dimitris giannopoulos <dimitris.giannopoulos@hotmail.com>
2022-11-09 13:58:56 +0200

    Created proper N to N relationship between Books and Authors

--- src/Data/SeedDatabase.cs
+++ src/Data/SeedDatabase.cs
@@ -11,7 +11,8 @@ namespace BookCatalogueAPI.Data
                 // Makes the seeding idempotent
                 if (!db.Database.EnsureCreated()) return;
 
-                db.Authors.AddRange(
+                List<Author> authors = new List<Author>()
+                {
                     new Author ("Harper", "Lee"),
                     new Author ("J. D.", "Salinger"),
                     new Author ("F. Scott", "Fitzgerald"),
@@ -36,41 +37,47 @@ namespace BookCatalogueAPI.Data
                     new Author ("Margaret", "Atwood"),
                     new Author ("Alice", "Walker"),
                     new Author ("Margaret", "Mitchell")
-                );
+                };
 
-                db.Categories.AddRange(
+                db.Authors.AddRange(authors);
+
+                List<Category> categories = new List<Category>()
+                {
                     new Category { Name = "thriller"},
                     new Category { Name = "fiction"},
                     new Category { Name = "romance"},
                     new Category { Name = "satire"},
-                    new Category { Name = "high fantasy"});
+                    new Category { Name = "high fantasy"}
+                };
+
+                db.Categories.AddRange(categories);
 
                 db.Books.AddRange(
-                    new Book { Title = "To Kill a Mockingbird", Authors = new List<Author>(){ new Author("Harper", "Lee") }, Category = "thriller"},
-                    new Book { Title = "To Kill a Mockingbird 2", Authors = new List<Author>(){ new Author("Harper", "Lee")}, Category = "thriller"},
-                    new Book { Title = "The Catcher in the Rye", Authors = new List<Author>(){ new Author("J. D.", "Salinger")}, Category = "fiction"},
-                    new Book { Title = "The Great Gatsby", Authors = new List<Author>(){ new Author("F. Scott", "Fitzgerald")}, Category = "fiction"},
-                    new Book { Title = "Wuthering Heights", Authors = new List<Author>(){ new Author("Emily", "Brontë")}, Category = "romance"},
-                    new Book { Title = "Jane Eyre", Authors = new List<Author>(){ new Author("Charlotte", "Brontë")}, Category = "romance"},
-                    new Book { Title = "Catch-22", Authors = new List<Author>(){ new Author("Joseph", "Heller")}, Category = "satire"},
-                    new Book { Title = "One Hundred Years of Solitude", Authors = new List<Author>(){ new Author("Gabriel García", "Márquez")}, Category = "high fantasy"},
-                    new Book { Title = "Pride and Prejudice", Authors = new List<Author>(){ new Author("Jane", "Austen")}, Category = "romance"},
-                    new Book { Title = "Great Expectations", Authors = new List<Author>(){ new Author("Charles", "Dickens")}, Category = "fiction"},
-                    new Book { Title = "The Lord of the Rings", Authors = new List<Author>(){ new Author("J. R. R.", "Tolkien")}, Category = "high fantasy"},
-                    new Book { Title = "Beloved", Authors = new List<Author>(){ new Author("Toni", "Morrison")}, Category = "fiction"},
-                    new Book { Title = "The Grapes of Wrath", Authors = new List<Author>(){ new Author("John", "Steinbeck")}, Category = "fiction"},
-                    new Book { Title = "Anna Karenina", Authors = new List<Author>(){ new Author("Leo", "Tolstoy")}, Category = "fiction"},
-                    new Book { Title = "Crime and Punishment", Authors = new List<Author>(){ new Author("Fyodor", "Dostoevsky")}, Category = "thriller"},
-                    new Book { Title = "Lord of the Flies", Authors = new List<Author>(){ new Author("William", "Golding")}, Category = "fiction"},
-                    new Book { Title = "Animal Farm", Authors = new List<Author>(){ new Author("George", "Orwell")}, Category = "satire"},
-                    new Book { Title = "Things Fall Apart", Authors = new List<Author>(){ new Author("Chinua", "Achebe")}, Category = "fiction"},
-                    new Book { Title = "Don Quixote", Authors = new List<Author>(){ new Author("Miguel", "de Cervantes")}, Category = "satire"},
-                    new Book { Title = "Brave New World", Authors = new List<Author>(){ new Author("Aldous", "Huxley")}, Category = "fiction"},
-                    new Book { Title = "Invisible Man", Authors = new List<Author>(){ new Author("Ralph", "Ellison")}, Category = "fiction"},
-                    new Book { Title = "Fahrenheit 451", Authors = new List<Author>(){ new Author("Ray", "Bradbury")}, Category = "fiction"},
-                    new Book { Title = "The Handmaid's Tale", Authors = new List<Author>(){ new Author("Margaret", "Atwood")}, Category = "fiction"},
-                    new Book { Title = "The Color Purple", Authors = new List<Author>(){ new Author("Alice", "Walker")}, Category = "fiction"},
-                    new Book { Title = "Gone with the Wind", Authors = new List<Author>(){ new Author("Margaret", "Mitchell")}, Category = "romance"});
+                    new Book { Title = "To Kill a Mockingbird", Authors = new List<Author>(){ authors[0] }, Category = "thriller"},
+                    new Book { Title = "To Kill a Mockingbird 2", Authors = new List<Author>(){ authors[0] }, Category = "thriller"},
+                    new Book { Title = "The Catcher in the Rye", Authors = new List<Author>(){ authors[1] }, Category = "fiction"},
+                    new Book { Title = "The Great Gatsby", Authors = new List<Author>(){ authors[2] }, Category = "fiction"},
+                    new Book { Title = "Wuthering Heights", Authors = new List<Author>(){ authors[3] }, Category = "romance"},
+                    new Book { Title = "Jane Eyre", Authors = new List<Author>(){ authors[4] }, Category = "romance"},
+                    new Book { Title = "Catch-22", Authors = new List<Author>(){ authors[5] }, Category = "satire"},
+                    new Book { Title = "One Hundred Years of Solitude", Authors = new List<Author>(){ authors[6] }, Category = "high fantasy"},
+                    new Book { Title = "Pride and Prejudice", Authors = new List<Author>(){ authors[7] }, Category = "romance"},
+                    new Book { Title = "Great Expectations", Authors = new List<Author>(){ authors[8] }, Category = "fiction"},
+                    new Book { Title = "The Lord of the Rings", Authors = new List<Author>(){ authors[9] }, Category = "high fantasy"},
+                    new Book { Title = "Beloved", Authors = new List<Author>(){ authors[10] }, Category = "fiction"},
+                    new Book { Title = "The Grapes of Wrath", Authors = new List<Author>(){ authors[11] }, Category = "fiction"},
+                    new Book { Title = "Anna Karenina", Authors = new List<Author>(){ authors[12] }, Category = "fiction"},
+                    new Book { Title = "Crime and Punishment", Authors = new List<Author>(){ authors[13] }, Category = "thriller"},
+                    new Book { Title = "Lord of the Flies", Authors = new List<Author>(){ authors[14] }, Category = "fiction"},
+                    new Book { Title = "Animal Farm", Authors = new List<Author>(){ authors[15] }, Category = "satire"},
+                    new Book { Title = "Things Fall Apart", Authors = new List<Author>(){ authors[16] }, Category = "fiction"},
+                    new Book { Title = "Don Quixote", Authors = new List<Author>(){ authors[17] }, Category = "satire"},
+                    new Book { Title = "Brave New World", Authors = new List<Author>(){ authors[18] }, Category = "fiction"},
+                    new Book { Title = "Invisible Man", Authors = new List<Author>(){ authors[19] }, Category = "fiction"},
+                    new Book { Title = "Fahrenheit 451", Authors = new List<Author>(){ authors[20] }, Category = "fiction"},
+                    new Book { Title = "The Handmaid's Tale", Authors = new List<Author>(){ authors[21] }, Category = "fiction"},
+                    new Book { Title = "The Color Purple", Authors = new List<Author>(){ authors[22] }, Category = "fiction"},
+                    new Book { Title = "Gone with the Wind", Authors = new List<Author>(){ authors[23] }, Category = "romance"});
 
                 db.SaveChanges();
             }
